0d1cf000d4c46f863d70ef9d17144752d701191a
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2021-11-14 00:56:01 -0600

    found page done

---
+++ pages/categories.ts
@@ -0,0 +1,30 @@
+export const categories = [
+  {
+    "name": "personal",
+    "subcategories": [
+      "wallet",
+      "keys",
+      "purse",
+      "backpack",
+      "other"
+    ]
+  }, {
+    "name": "technology",
+    "subcategories": [
+      "laptop",
+      "calculator",
+      "phone",
+      "other"
+    ]
+  }, {
+    "name": "clothing",
+    "subcategories": [
+      "jacket",
+      "sweater",
+      "shoes",
+      "other"
+    ]
+  }
+]
+
+export default categories;
