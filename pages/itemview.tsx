6bb0fe28de0cce1a5ce9e770d853d4a7e1615659
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2021-11-13 21:46:58 -0600

    make stub pages

---
+++ pages/itemview.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import styles from "../styles/ItemView.module.css";
+
+// should redirect to login page if not logged in
+
+const ItemView: NextPage = () => {
+  return (
+    <div>
+      <div className="">
+        <h1>ItemView</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ItemView;
---
+++ pages/profile.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import styles from "../styles/Profile.module.css";
+
+// should redirect to login page if not logged in
+
+const Profile: NextPage = () => {
+  return (
+    <div>
+      <div className="">
+        <h1>Profile</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
